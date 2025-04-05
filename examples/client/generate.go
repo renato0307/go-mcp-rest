f68cea9e0dd2d3152f8482051c8774b2b549b0d5
Renato Torres <renato.torres@outsystems.com>
2025-04-05 10:29:59 +0100

    feat: add simple example

---
+++ examples/client/generate.go
@@ -0,0 +1,3 @@
+package client
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config cfg.yaml https://converter.swagger.io/api/convert?url=https://eng-test-us-01-dev.outsystems.app/MCPBackend/rest/Backend/swagger.json
